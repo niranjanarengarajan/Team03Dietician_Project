80594c915eddad11e42b536b76e9e5613f5d3b05
ambiga87 <ambiga87.it@gmail.com>
2026-03-24 16:45:27 -0400

    Cleared the in Hooks

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -3,10 +3,7 @@ package stepDefinitions;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import utils.Screenshot;
 import driverManager.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
@@ -14,50 +11,45 @@ import io.cucumber.java.Scenario;
 import utils.ConfigReader;
 import utils.ExcelReader;
 import utils.LoggerLoad;
-
+import utils.Screenshot;
 
 public class Hooks {
-	private WebDriver driver;
-	
-//	private static final Logger logger = LoggerFactory.getLogger(Hooks.class);
+    private WebDriver driver;
 
-	@Before(order=0)
-	public void setup() {
-		
-		LoggerLoad.info("Initializing test setup...");
-		Properties prop = ConfigReader.initializeProperties();
-		LoggerLoad.info("Loaded configuration properties");
-		ExcelReader.readDataFromExcel(prop.getProperty("loginsheetName"));
-		ExcelReader.readDataFromExcel(prop.getProperty("AddPatientSheetName"));		
-	}
-		DriverFactory.launchBrowser(prop.getProperty("browserName"));
-		DriverFactory.getDriver().get(prop.getProperty("baseURL"));
-		LoggerLoad.info("Navigated to base URL: "+ prop.getProperty("baseURL"));
-		
-	}
-	
-	@Before(value = "@Login", order = 1)
-	public void performLogin() {
-		Login_Step login_Step = new Login_Step();
-//		pom = new PageObjectManager();
-		login_Step.user_clicks_login_button_after_entering_valid_credentials_in_login_page();
-		LoggerLoad.info("Performed login with valid credentials");
-	}
+    @Before(order=0)
+    public void setup() {
+        LoggerLoad.info("Initializing test setup...");
+        
+        Properties prop = ConfigReader.initializeProperties();
+        LoggerLoad.info("Loaded configuration properties");        
+       
+        ExcelReader.readDataFromExcel(prop.getProperty("loginsheetName"));
+        ExcelReader.readDataFromExcel(prop.getProperty("AddPatientSheetName"));        
+        
+        DriverFactory.launchBrowser(prop.getProperty("browserName"));
+        DriverFactory.getDriver().get(prop.getProperty("baseURL"));
+        LoggerLoad.info("Navigated to base URL: " + prop.getProperty("baseURL"));
+    }
 
-    
-	@After
-	public void tearDown(Scenario scenario) {
-		 if (scenario.isFailed()) {
-	        	Screenshot.takeScreenshot(DriverFactory.getDriver(),scenario);
-	            LoggerLoad.info("Screenshot captured for failed scenario: " + scenario.getName());
-	        }
-		 
-		if (DriverFactory.getDriver() != null) {
-			LoggerLoad.info("Tearing down WebDriver and closing browser");
-			DriverFactory.getDriver().quit();
-			DriverFactory.mydriver.remove();
-			LoggerLoad.info("Driver removed from ThreadLocal");
-		}
-	}
+    @Before(value = "@Login", order = 1)
+    public void performLogin() {
+        Login_Step login_Step = new Login_Step();
+        login_Step.user_clicks_login_button_after_entering_valid_credentials_in_login_page();
+        LoggerLoad.info("Performed login with valid credentials");
+    }
 
+    @After
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            Screenshot.takeScreenshot(DriverFactory.getDriver(), scenario);
+            LoggerLoad.info("Screenshot captured for failed scenario: " + scenario.getName());
+        }
+
+        if (DriverFactory.getDriver() != null) {
+            LoggerLoad.info("Tearing down WebDriver and closing browser");
+            DriverFactory.getDriver().quit();
+            DriverFactory.mydriver.remove();
+            LoggerLoad.info("Driver removed from ThreadLocal");
+        }
+    }
 }
