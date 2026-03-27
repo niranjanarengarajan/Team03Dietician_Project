46ba1264f858a0bf2f32d50f39720168c5987068
Christy <j.christypriya@gmail.com>
2026-03-27 01:11:31 -0500

    updated testdata,config

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 
         features = "src/test/resources/FeatureFiles", 
-        tags= "@loginPage or @dashboardPage or @viewTestReportPage",
+//        tags= "@loginPage or @dashboardPage or @viewTestReportPage",
         glue = "stepDefinitions", 
         plugin = {
                 "pretty",
